280807a2a90deec5e59bde844d7505b44d990eda
Romain Vimont <rom@rom1v.com>
2023-02-24 23:05:24 +0100

    Initial sample

---
+++ sample.c
@@ -0,0 +1,28 @@
+#include <SDL2/SDL.h>
+
+int main(void) {
+    if (SDL_Init(SDL_INIT_AUDIO)) {
+        fprintf(stderr, "SDL_Init failed: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_AudioSpec desired = {
+        .freq = 48000,
+        .format = AUDIO_S16LSB,
+        .channels = 2,
+        .samples = 4096,
+    };
+    SDL_AudioSpec obtained;
+
+    SDL_AudioDeviceID device =
+        SDL_OpenAudioDevice(NULL, 0, &desired, &obtained, 0);
+    printf("device = %d\n", device);
+    if (!device) {
+        fprintf(stderr, "%s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_CloseAudioDevice(device);
+
+    return 0;
+}
